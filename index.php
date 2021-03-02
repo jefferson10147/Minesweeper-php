448b10c13fd2b9a62bd7d578c6d72adb30268121
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-02 08:12:48 -0400

    Creating form to fill matrix

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+</head>
+<body>
+    
+    <form action="start_game.php" method="get">
+        <label>Por favor escoja un número entre 8 y 20</label>
+        <input type="number" min="8" max="20" name="size">
+        <button type="submit" value="send">ok</button>
+    </form>
+
+</body>
+</html>
