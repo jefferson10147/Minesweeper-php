aefcb64fe2da5e3937691b6b25d39d885f5d6c9a
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-03-02 21:35:45 -0400

    Creating best scores table

--- win_page.php
+++ win_page.php
@@ -1,7 +1,7 @@
 <?php
 include("./scores_model.php");
 
-session_start(); 
+session_start();
 if (isset($_SESSION["gameTime"]) && isset($_SESSION["size"]) && isset($_SESSION["username"])) {
     $time = $_SESSION["gameTime"];
     $level = $_SESSION["size"];
@@ -10,6 +10,9 @@ if (isset($_SESSION["gameTime"]) && isset($_SESSION["size"]) && isset($_SESSION[
 
 $scoresModel = new Scores();
 $scoresModel->insertScore($username, $time, $level);
+
+$_SESSION["gameTime"] = null;
+$scores = $scoresModel->getAllScores();
 ?>
 
 <!DOCTYPE html>
@@ -20,14 +23,55 @@ $scoresModel->insertScore($username, $time, $level);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>You win</title>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
 </head>
 
 <body>
     <?php
-
-    echo "<h2>Felicitaciones " . $username . " Ha ganado en el nivel " . $level . " in " . $time . " seconds.";
-
+    echo "<h2>Felicitaciones " . $username . " Ha ganado en el nivel " . $level . " in " . $time . " segundos.</h2>";
     ?>
+
+    <h2>
+        Mejores Resultados:
+    </h2>
+
+    <table>
+        <tr>
+            <th>Jugador</th>
+            <th>Tiempo (s)</th>
+            <th>Nivel</th>
+        </tr>
+
+        <?php
+
+        foreach ($scores as $score) {
+            echo "<tr>";
+            echo "<td>" . $score["name"] . "</td>";
+            echo "<td>" . $score["time"] . "</td>";
+            echo "<td>" . $score["level"] . "</td>";
+        }
+
+        ?>
+    </table>
+
+    <br>
     <button><a href="./close_session.php">Nueva Partida</a></button>
 </body>
 
